b9e8be09085619d27b959609b6fd4fa497e5002b
CodeMaster2004 <falexanderhp@gmail.com>
2025-04-11 08:59:26 -0500

    Actualizacion semana03

--- Assets/Ninja/NewMonoBehaviourScript.cs
+++ Assets/Ninja/NewMonoBehaviourScript.cs
@@ -3,19 +3,46 @@ using UnityEngine;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     Rigidbody2D rb;
+    SpriteRenderer sr;
+    Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr= GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MoverHorizontal();
+        Saltar();
+       
+    }
+    void MoverHorizontal()
+    {
+        animator.SetInteger("estado", 0);
         rb.linearVelocityX = 0;
-        if(Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             rb.linearVelocityX = 5;
+            sr.flipX = false;
+            animator.SetInteger("Estado ", 1);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.linearVelocityX = -5;
+            sr.flipX = true;
+            animator.SetInteger("Estado ", 1);
+
+        }
+
+    }
+    void Saltar()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            rb.linearVelocityY = 12.5f;
         }
     }
 }
